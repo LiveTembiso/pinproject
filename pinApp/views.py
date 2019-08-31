ae21fc3b84952a5ee337214c19d75ba9288a47b9
livet <livetembiso0@gmail.com>
2019-08-31 23:06:46 +0200

    Context scope fixed

--- pinApp/views.py
+++ pinApp/views.py
@@ -3,54 +3,35 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from pinApp.forms import SimpleForm
 
-myList = []
-length = 0
-another = 0
 global counter
 counter = 0
-stringList = []
+another = 0
+length = 0
+myList = []
 intList = []
-values = []
-
-def method(a,l):
-	if a[l:l+1] in myList:
-		global counter
-		counter = counter + 1
-
-	if counter == length:
-		global another
-		another += 1
-		values.append(a)
 
 def index(request):
 	#return HttpResponse("Hello World!")
 	myForm = SimpleForm(request.POST or None)
 
+	global context
+	context = {"form":myForm}
 	if request.POST.get('submit') == 'submit':
 		print('Submit clicked')
 
 	if myForm.is_valid():
 		length = (int)(myForm.cleaned_data.get("length"))
 		myList = request.POST.getlist("options")
-		
-		stringList = myList.copy()
 
-		for a in myList:
-			temp = (int)(a)
+		for num in myList:
+			temp = (int)(num)
 			intList.append(temp)
+		
 		#print(myList)
-		first = (int)(myList[0]*length)
-		last = (int)(myList[len(myList)-1]*length)
-
-		for i in range(first,last+1):
-			str_x = str(i)
-			global counter
-			counter = counter - counter
-			for m in range(length):
-				if method(str_x,m):
-					continue
-
-	context = {'form':myForm,'values':values}
+		#first = (int)(myList[0]*length)
+		#last = (int)(myList[len(myList)-1]*length)
+		
+		context = {'form':myForm,'length':length, 'myList':myList}
 
 	return render(request,"home.html",context)
 
