93a2a239d4357b59e796300c81a727c772349a1a
livet <livetembiso0@gmail.com>
2019-09-01 01:33:48 +0200

    App works fine

--- pinApp/views.py
+++ pinApp/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 from pinApp.forms import SimpleForm
 
 global counter
-counter = 0
-another = 0
+global another
 length = 0
 myList = []
 intList = []
@@ -14,6 +13,8 @@ def index(request):
 	#return HttpResponse("Hello World!")
 	myForm = SimpleForm(request.POST or None)
 
+	counter = 0
+	pinCount = 0
 	global context
 	context = {"form":myForm}
 	if request.POST.get('submit') == 'submit':
@@ -22,16 +23,39 @@ def index(request):
 	if myForm.is_valid():
 		length = (int)(myForm.cleaned_data.get("length"))
 		myList = request.POST.getlist("options")
+		myOtherList = []
 
-		for num in myList:
+		first = (int)(myList[0]*length)
+		last = (int)(myList[len(myList)-1]*length)
+
+		index = 0;
+		for pin in range(first,last+1):
+			str_pin = str(pin).zfill(length)
+			counter -= counter
+
+			for digit in range(length):
+				if str_pin[digit:digit+1] in myList:
+					counter = counter + 1
+
+				if counter == length:
+					
+					print(str_pin)
+					myOtherList.append(str_pin);
+					pinCount += 1
+
+
+		'''for num in myList:
 			temp = (int)(num)
-			intList.append(temp)
+			intList.append(temp)'''
+
+
 		
 		#print(myList)
 		#first = (int)(myList[0]*length)
 		#last = (int)(myList[len(myList)-1]*length)
 		
-		context = {'form':myForm,'length':length, 'myList':myList}
+		context = {'form':myForm,'length':pinCount, 'myList':myOtherList}
+
 
 	return render(request,"home.html",context)
 
